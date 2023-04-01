c2513a26c133148c11169876b3e3df819ac70dfc
jazsouf <101473962+jazsouf@users.noreply.github.com>
2023-04-01 22:32:26 +0200

    solve frist four exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...Args) {
+    newArray = array.slice();
+    for (let i = 0; i < array.length; i++) {
+        for (const arg of Args) {
+            if (array[i] === arg) {
+                let removeElement = newArray.indexOf(array[i])
+                newArray.splice(removeElement, 1); 
+            }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
